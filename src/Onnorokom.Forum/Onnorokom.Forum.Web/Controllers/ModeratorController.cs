0749c622b3952c2d1908f14543d8cb3fd65469fa
Arafin Ahmed <arafin2021@gmail.com>
2022-01-12 20:57:32 +0600

    Layout changes.

---
+++ src/Onnorokom.Forum/Onnorokom.Forum.Web/Controllers/ModeratorController.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Onnorokom.Forum.Membership.Entities;
+
+namespace Onnorokom.Forum.Web.Controllers
+{
+    [Authorize(Policy = "Moderator")]
+    public class ModeratorController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<AccountController> _logger;
+        private readonly ILifetimeScope _scope;
+
+        public ModeratorController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILifetimeScope scope,
+            ILogger<AccountController> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _scope = scope;
+        }
+
+        public IActionResult CreateBoard()
+        {
+            return View();
+        }
+    }
+}
