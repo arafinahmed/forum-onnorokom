8c1bc084b72990c1cff19f042b18d95083e2ce76
Arafin Ahmed <arafin2021@gmail.com>
2022-01-14 17:59:38 +0600

    Unused code removed.

--- src/Onnorokom.Forum/Onnorokom.Forum.DataAccessLayer/Repositories/IRepository.cs
+++ src/Onnorokom.Forum/Onnorokom.Forum.DataAccessLayer/Repositories/IRepository.cs
@@ -12,28 +12,8 @@ namespace Onnorokom.Forum.DataAccessLayer.Repositories
     {
         Task AddAsync(TEntity entity);
         void Remove(TEntity entityToDelete);
-        void Remove(TKey Id);
-        void Remove(Expression<Func<TEntity, bool>> expression);
-        void Edit(TEntity entityToUpdate);
-        int GetCount(Expression<Func<TEntity, bool>> expression = null);
         IList<TEntity> Get(Expression<Func<TEntity, bool>> expression, string includeProperties = "");
         IList<TEntity> GetAll();
         TEntity GetById(TKey Id);
-        (IList<TEntity> data, int total, int totalDisplay) Get(
-            Expression<Func<TEntity, bool>> expression = null,
-            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
-            string includeProperties = "", int pageIndex = 1, int pageSize = 10,
-            bool isTrackingOff = false);
-        //(IList<TEntity> data, int total, int totalDisplay) GetDynamic(
-        //    Expression<Func<TEntity, bool>> expression = null,
-        //    string orderBy = null,
-        //    string includeProperties = "", int pageIndex = 1, int pageSize = 10,
-        //    bool isTrackingOff = false);
-        IList<TEntity> Get(Expression<Func<TEntity, bool>> expression = null,
-            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
-            string includeProperties = "", bool isTrackingOff = false);
-        //IList<TEntity> GetDynamic(Expression<Func<TEntity, bool>> expression = null,
-        //    string orderBy = null,
-        //    string includeProperties = "", bool isTrackingOff = false);
     }
 }
