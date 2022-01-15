a36c7b32f45f5bda424794b7de47d0ab43c32ed9
Arafin Ahmed <arafin2021@gmail.com>
2022-01-16 01:06:53 +0600

    Bug fix in password match.

--- src/Onnorokom.Forum/Onnorokom.Forum.Web/Models/Account/RegisterModel.cs
+++ src/Onnorokom.Forum/Onnorokom.Forum.Web/Models/Account/RegisterModel.cs
@@ -6,20 +6,22 @@ namespace Onnorokom.Forum.Web.Models.Account
 {
     public class RegisterModel
     {
-        [Required]
+        [Required(ErrorMessage = "Provide a valid email.")]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please provide a new password")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please provide a confirmation password")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The new password and confirmation password do not match.")]
+
         public string ConfirmPassword { get; set; }
         public string? ReturnUrl { get; set; }
         public IList<AuthenticationScheme>? ExternalLogins { get; set; }
